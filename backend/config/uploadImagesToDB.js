457c465716a1266d44102ef48948501fe79d9568
apolonio420 <vwnrvm@gmail.com>
2023-11-15 18:21:44 -0300

    Changed to add embbeddings to images for better analysis

--- backend/config/uploadImagesToDB.js
+++ backend/config/uploadImagesToDB.js
@@ -1,7 +1,31 @@
 const connectDB = require('./connectMongo');
 const fs = require('fs');
+const axios = require('axios');
 const Image = require('../models/Image');
 const path = require('path');
+require('dotenv').config({ path: path.join(__dirname, '..', '..', '.env') });
+
+
+// Función para obtener el embedding de OpenAI
+async function getEmbeddingForDescription(description) {
+  try {
+    const response = await axios.post('https://api.openai.com/v1/embeddings', {
+      model: "text-embedding-ada-002", // Utilizamos el modelo adecuado
+      input: description
+    }, {
+      headers: {
+        'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
+        'Content-Type': 'application/json'
+      }
+    });
+
+    // Aquí asumimos que la respuesta contiene un array con los embeddings y tomamos el primero
+    return response.data.data[0].embedding; 
+  } catch (error) {
+    console.error('Error al obtener el embedding:', error);
+    throw error;
+  }
+}
 
 async function uploadImagesToDB() {
   await connectDB();
@@ -11,16 +35,20 @@ async function uploadImagesToDB() {
   const images = JSON.parse(data);
 
   for (const image of images) {
+    // Obtener el embedding para la descripción de la imagen
+    const embedding = await getEmbeddingForDescription(image.description);
+
     const newImage = new Image({
       imageUrl: image.imageUrl,
       description: image.description,
-      niche: image.niche // Utiliza el campo niche
+      niche: image.niche,
+      embedding: embedding // Añadimos el embedding al documento
     });
 
     await newImage.save();
   }
 
-  console.log('Todas las imágenes se han guardado exitosamente en la base de datos.');
+  console.log('Todas las imágenes se han guardado exitosamente en la base de datos con sus embeddings.');
 }
 
 uploadImagesToDB().catch(console.error);
